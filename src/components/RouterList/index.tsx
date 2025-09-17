d6e96d30ab62d82760399ee0061b6d76f2825a3e
宇阳 <liuyuyang1024@yeah.net>
2025-09-17 15:22:56 +0800

    添加对 @ant-design/v5-patch-for-react-19 和 antd 的依赖，重构路由结构，使用 RouterList 组件替代原有的 BrowserRouter 和 Routes 结构。

--- src/components/RouterList/index.tsx
+++ src/components/RouterList/index.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Layout from '@/pages/layout';
+import Home from '@/pages/home';
+
 export default () => {
-  return <h1>Hello</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
-import Home from '@/pages/home';
+import RouterList from './components/RouterList';
+import '@ant-design/v5-patch-for-react-19';
 import './styles/global.css';
 
 const app = createRoot(document.getElementById('root')!);
-app.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
-);
+app.render(<RouterList />);
