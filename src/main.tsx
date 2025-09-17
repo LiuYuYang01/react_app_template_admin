0ab00a29628057fdeee97fb65b6d9df92615bee2
宇阳 <liuyuyang1024@yeah.net>
2025-09-17 15:19:02 +0800

    移除对 @heroui/react 的依赖，并简化 main.tsx 中的组件结构，删除不再使用的 hero.ts 文件。

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { HeroUIProvider } from '@heroui/react';
 import Home from '@/pages/home';
 import './styles/global.css';
 
 const app = createRoot(document.getElementById('root')!);
 app.render(
-  <HeroUIProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </HeroUIProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 );
